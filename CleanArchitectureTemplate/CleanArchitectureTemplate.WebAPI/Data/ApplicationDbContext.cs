4ba0dd0d87e5deda5c6019826d48e11c338e9523
balexandrov <balexandrov@topviewnyc.com>
2021-08-26 01:09:26 +0300

    Added custom db context SaveChanges.

--- CleanArchitectureTemplate/CleanArchitectureTemplate.WebAPI/Data/ApplicationDbContext.cs
+++ CleanArchitectureTemplate/CleanArchitectureTemplate.WebAPI/Data/ApplicationDbContext.cs
@@ -1,10 +1,56 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    private readonly IMediator _mediator;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IMediator mediator)
+            : base(options)
     {
+        _mediator = mediator;
+    }
+
+    //public DbSet<AspNetUsers> AspNetUsers { get; set; }
+    //public DbSet<AspNetRoles> AspNetRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+        // ignore events if no dispatcher provided
+        if (_mediator == null) return result;
+
+        // dispatch events only if save was successful
+        //var entitiesWithEvents = ChangeTracker.Entries<BaseEntity>()
+        //    .Select(e => e.Entity)
+        //    .Where(e => e.Events.Any())
+        //    .ToArray();
+
+        //foreach (var entity in entitiesWithEvents)
+        //{
+        //    var events = entity.Events.ToArray();
+        //    entity.Events.Clear();
+        //    foreach (var domainEvent in events)
+        //    {
+        //        await _mediator.Publish(domainEvent).ConfigureAwait(false);
+        //    }
+        //}
+
+        return result;
+    }
+
+    public override int SaveChanges()
+    {
+        return SaveChangesAsync().GetAwaiter().GetResult();
     }
 }
